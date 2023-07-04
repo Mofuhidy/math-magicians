b62255caefa6366ab8779a89e41274c96b13ab90
moFuhidy <mo7ammed9290@gmail.com>
2023-07-04 17:19:33 +0300

    Add the click event handler

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,21 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-function Buttons({ symbol, btnColor }) {
+function Buttons({ symbol, btnColor, handleOnClick }) {
   const regularClass = `py-4  ${btnColor}`;
   const uniqueClass = `py-4 col-span-2 ${btnColor}`;
   return (
 
     symbol === 0
-      ? <button type="button" className={uniqueClass}>{symbol}</button>
-      : <button type="button" className={regularClass}>{symbol}</button>
+      ? <button type="button" className={uniqueClass} onClick={handleOnClick}>{symbol}</button>
+      : <button type="button" className={regularClass} onClick={handleOnClick}>{symbol}</button>
 
   );
 }
 
 Buttons.propTypes = {
-  symbol: PropTypes.isRequired,
-  btnColor: PropTypes.isRequired,
+  symbol: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
+  btnColor: PropTypes.string.isRequired,
+  handleOnClick: PropTypes.func.isRequired,
 };
 
 export default Buttons;
--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,19 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Buttons from './Buttons';
+import calculate from '../logic/calculate';
 
 const calcSymbols = ['AC', '+/-', '%', '÷', 7, 8, 9, 'x', 4, 5, 6, '-', 1, 2, 3, '+', 0, '.', '='];
 
 export default function Calculator() {
+  const [calculator, setCalculator] = useState({
+    total: null,
+    next: null,
+    operation: null,
+  });
+
+  const handleOnClick = (e) => {
+    const buttonName = e.target.innerText;
+    setCalculator(calculate(calculator, buttonName));
+  };
+
+  const { total, next, operation } = calculator;
+
   return (
     <div className=" w-1/2 h-fi bg-amber-100 rounded-lg shadow-sm">
       <div className="display text-right text-gray-700">
-        <h1 className="number text-3xl pr-5 pt-5 pb-5 ">0</h1>
+
+        <h1 className="number text-3xl pr-5 pt-5 pb-5 ">
+          {total || next || operation || 0}
+        </h1>
       </div>
       <div className="grid grid-cols-4 divide-x divide-y">
         {calcSymbols.map((el) => (el === 'x' || el === '÷' || el === '+' || el === '-' || el === '.' || el === '='
-          ? <Buttons symbol={el} key={el} btnColor="bg-[#F8CBA6] text-white hover:text-gray-700 hover:bg-amber-50 " />
-          : <Buttons symbol={el} key={el} btnColor="bg-amber-50 text-gray-700  hover:text-white hover:bg-[#F8CBA6]" />))}
+          ? <Buttons symbol={el} key={el} btnColor="bg-[#F8CBA6] text-white hover:text-gray-700 hover:bg-amber-50 " handleOnClick={handleOnClick} />
+          : <Buttons symbol={el} key={el} btnColor="bg-amber-50 text-gray-700  hover:text-white hover:bg-[#F8CBA6]" handleOnClick={handleOnClick} />))}
       </div>
+
     </div>
   );
 }
