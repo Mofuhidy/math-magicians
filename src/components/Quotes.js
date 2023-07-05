92668b19c9a7d69751aa02e18582e688e3644b22
moFuhidy <mo7ammed9290@gmail.com>
2023-07-05 13:57:46 +0300

    Add loading handler

--- src/components/Quotes.js
+++ src/components/Quotes.js
@@ -2,9 +2,10 @@ import React, { useEffect, useState } from 'react';
 
 function Quotes() {
   const [quote, setQuote] = useState(null);
-  
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
+    setIsLoading(true);
     const fetchRandomQuote = async () => {
       const response = await fetch('https://api.api-ninjas.com/v1/quotes', {
         headers: {
@@ -13,15 +14,14 @@ function Quotes() {
       });
       const jsonData = await response.json();
       setQuote(jsonData[0].quote);
+      setIsLoading(false);
     };
     fetchRandomQuote();
   }, []);
 
   return (
     <div className="col-span-1 flex flex-col items-center justify-center w-3/4">
-
-      <p>{quote}</p>
-
+      { (isLoading ? <p>Loading ...</p> : <p>{quote}</p>)}
     </div>
   );
 }
