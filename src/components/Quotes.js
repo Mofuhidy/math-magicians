46f697e2f92b691e1e9a195d9728b3d2f7c4b16d
moFuhidy <mo7ammed9290@gmail.com>
2023-07-05 13:54:41 +0300

    Fetch data from API

--- src/components/Quotes.js
+++ src/components/Quotes.js
@@ -1,13 +1,27 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 function Quotes() {
   const [quote, setQuote] = useState(null);
   
 
+  useEffect(() => {
+    const fetchRandomQuote = async () => {
+      const response = await fetch('https://api.api-ninjas.com/v1/quotes', {
+        headers: {
+          'X-Api-Key': 'tvkUus/p9WHwSOdk+kbWHA==a2jd4Ger5SzgFZAb',
+        },
+      });
+      const jsonData = await response.json();
+      setQuote(jsonData[0].quote);
+    };
+    fetchRandomQuote();
+  }, []);
+
   return (
-    <div className="col-span-1 grid-flow-row grid place-items-center">
-      <h1 className="text-lg">Quotes</h1>
+    <div className="col-span-1 flex flex-col items-center justify-center w-3/4">
+
       <p>{quote}</p>
+
     </div>
   );
 }
