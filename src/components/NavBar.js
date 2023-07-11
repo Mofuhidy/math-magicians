19b68938c37eaf0587ba7180bb7f64c62a4ecd2d
moFuhidy <mo7ammed9290@gmail.com>
2023-07-11 14:29:34 +0300

    Add navMenu function

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,11 +1,18 @@
+import { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 
 function NavBar() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
   const links = [
     { path: '/', name: 'Home' },
     { path: 'Calc', name: 'Calculator' },
     { path: '/Quote', name: 'Quote' },
   ];
+
+  const handleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
 
   return (
     <nav className=" flex justify-between px-5 items-center h-16 mb-8  bg-amber-50 shadow-sm">
@@ -13,11 +20,33 @@ function NavBar() {
         Math
         <span className="ml-2 text-[#ffb579]">Magicians</span>
       </h1>
-      <ul className="flex justify-around">
+
+      <div className="sm:hidden">
+        <button
+          type="button"
+          className="text-dark font-semibold"
+          onClick={handleMenu}
+        >
+          Menu
+        </button>
+      </div>
+
+      <ul className={`flex ${
+        isMenuOpen ? 'block' : 'hidden'
+      } sm:flex sm:justify-around sm:items-center`}
+      >
         {
         links.map((link) => (
           <li key={link.name} className=" mx-2">
-            <NavLink className="text-dark font-semibold aria-[current=page]:text-[#ffb579] hover:underline underline-offset-8 hover:text-[#ffb579]" to={link.path}>{link.name}</NavLink>
+            <NavLink
+              className="text-dark font-semibold aria-[current=page]:text-[#ffb579]
+               hover:underline underline-offset-8 hover:text-[#ffb579]"
+              to={link.path}
+              onClick={handleMenu}
+            >
+              {link.name}
+
+            </NavLink>
           </li>
         ))
        }
